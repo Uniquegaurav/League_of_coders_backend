cbc5ef8fa9942b47ecc45772feda5308447fd173
Kumar Gaurav <k.gauravsingh.9234.gs@gmail.com>
2023-03-06 01:11:59 +0530

    cors upddate

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ dotenv.config();
 
 app.use(bodyParser.json({limit : "30mb",extended : true}));
 app.use(bodyParser.urlencoded({limit : "30mb",extended : true}));
-app.use(cors());
+app.use(cors({
+       origin: '*'
+   }));
 
 // adding post routes
 app.use('/posts',postRoutes);
